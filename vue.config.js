599621afdcf0718a075216bf62a1681f246058a9
Татьяна Хлопенкова <tanyahlopenkova@gmail.com>
2023-10-27 13:09:25 +0300

    fixes for deploy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   pwa: {
-    name: "PWA app example",
-    short_name: "PWA app",
-    themeColor: "#98c1d9",
+    workboxPluginMode: 'GenerateSW',
+    manifestOptions: {
+      name: "PWA app example",
+      short_name: "PWA app",
+      themeColor: "#98c1d9",
+    }
   }
 };
